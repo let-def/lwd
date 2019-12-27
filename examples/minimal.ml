c2bad269fddd558c9872d21ce2955c84cff41bd7
Frédéric Bour <fred@tarides.com>
2019-12-27 22:45:15 +0100

    celsius-to-farenheit

--- examples/minimal.ml
+++ examples/minimal.ml
@@ -2,9 +2,7 @@ open Nottui
 
 (* Put the UI here *)
 
-let bind x f = Lwd.join (Lwd.map f x)
-
-let node title ~f =
+(*let node title ~f =
   let vopened = Lwd.var false in
   let label =
     Lwd.map' (Lwd.get vopened) @@ fun opened ->
@@ -30,7 +28,57 @@ let rec count_to_10 () =
       [1;2;3;4;5;6;7;8;9;10]
   )
 
-let root = count_to_10 ()
+let root = count_to_10 ()*)
+
+let f_to_c x = (x -. 32.0) *. 5.0/.9.0
+let c_to_f x = x *. 9.0/.5.0 +. 32.0
+
+let degrees = Lwd.var 0.0
+
+let farenheit = Lwd.var (nan, ("", 0))
+
+let farenheit_text =
+  Lwd.map2' (Lwd.get degrees) (Lwd.get farenheit)
+    (fun d (d', f) ->
+       if d = d' then f else (string_of_float (c_to_f d), 0))
+
+let farenheit_edit =
+  Nottui_widgets.edit_field
+    farenheit_text
+    ~on_change:(fun (text, _ as state) ->
+        let d = match float_of_string_opt text with
+          | None -> Lwd.peek degrees
+          | Some d -> let d = f_to_c d in Lwd.set degrees d; d
+        in
+        Lwd.set farenheit (d, state)
+      )
+    ~on_submit:ignore
+
+let celsius = Lwd.var (nan, ("", 0))
+
+let celsius_text =
+  Lwd.map2' (Lwd.get degrees) (Lwd.get celsius)
+    (fun d (d', f) -> if d = d' then f else (string_of_float d, 0))
+
+let celsius_edit =
+  Nottui_widgets.edit_field
+    celsius_text
+    ~on_change:(fun (text, _ as state) ->
+        let d = match float_of_string_opt text with
+          | None -> Lwd.peek degrees
+          | Some d -> Lwd.set degrees d; d
+        in
+        Lwd.set celsius (d, state)
+      )
+    ~on_submit:ignore
+
+let root =
+  Lwd_utils.pack Ui.pack_y [
+    Lwd.pure (Nottui_widgets.string "Celsius:");
+    celsius_edit;
+    Lwd.pure (Nottui_widgets.string "Farenheight:");
+    farenheit_edit;
+  ]
 
 (*let () = Statmemprof_emacs.start 1E-4 30 5*)
 
