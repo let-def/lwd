06d1146abcf80d2ac36e38add95af4c5673eaf9e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-03-11 14:17:00 -0500

    add `vlist_with` for a more powerful vertical list

--- lib/nottui-widgets/nottui_widgets.ml
+++ lib/nottui-widgets/nottui_widgets.ml
@@ -399,6 +399,28 @@ let vlist (l: Ui.t Lwd.t list) : Ui.t Lwd.t =
   |> List.map (fun ui -> Lwd.map (Ui.join_x (string "- ")) ui)
   |> Lwd_utils.pack Ui.pack_y
 
+(** A list of items with a dynamic filter on the items *)
+let vlist_with
+    ?(filter=Lwd.return (fun _ -> true))
+    (f:'a -> Ui.t Lwd.t)
+    (l:'a list Lwd.t) : Ui.t Lwd.t =
+  let open Lwd.Infix in
+  let rec filter_map_ acc f l =
+    match l with
+    | [] -> List.rev acc
+    | x::l' ->
+      let acc' = match f x with | None -> acc | Some y -> y::acc in
+      filter_map_ acc' f l'
+  in
+  let l = l >|= List.map (fun x -> x, Lwd.map (Ui.join_x (string "- ")) @@ f x) in
+  let l_filter : _ list Lwd.t =
+    filter >>= fun filter ->
+    l >|=
+    filter_map_ []
+      (fun (x,ui) -> if filter x then Some ui else None)
+  in
+  l_filter >>= Lwd_utils.pack Ui.pack_y
+
 let button ?attr s f =
   Ui.mouse_area (fun ~x:_ ~y:_ _ -> f(); `Handled) (string ?attr s)
 
