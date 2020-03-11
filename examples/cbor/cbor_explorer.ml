75d01c0b8fc10349f74ef7fd865cc2fffd545a33
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-03-11 17:57:38 -0500

    update cbor_explorer example

--- examples/cbor/cbor_explorer.ml
+++ examples/cbor/cbor_explorer.ml
@@ -3,30 +3,6 @@ module W = Nottui_widgets
 module C = CBOR.Simple
 module A = Notty.A
 
-let unfoldable summary (f: unit -> Ui.Ui.t Lwd.t) : Ui.Ui.t Lwd.t =
-  let opened = ref false in
-  let v = Lwd.var W.empty_lwd in
-  let cursor ~x:_ ~y:_ = function
-     | `Left when !opened ->
-       opened := false;
-       Lwd.set v W.empty_lwd;
-       `Handled
-     | `Left ->
-       opened := true;
-       (* call [f] and pad a bit *)
-       let inner =
-         f()
-         |> Lwd.map (fun x -> Ui.Ui.join_x (W.string "> ") x)
-       in
-       Lwd.set v @@  inner;
-       `Handled
-     | _ -> `Unhandled
-  in
-  let mouse =
-    Lwd.map (fun m -> Ui.Ui.mouse_area cursor m) summary
-  in
-  Lwd_utils.pack Ui.Ui.pack_x [mouse; Lwd.join @@ Lwd.get v]
-
 let ui_of_cbor (c:C.t) =
   let quit = Lwd.var false in
   let w_q = W.main_menu_item "[quit]" (fun () -> Lwd.set quit true; W.empty_lwd) in
@@ -44,7 +20,7 @@ let ui_of_cbor (c:C.t) =
     | `Array l ->
       if fold then (
         let summary = Lwd.return @@ W.printf ~attr:A.(fg yellow) "<array(%d)>" (List.length l) in
-        unfoldable summary
+        W.unfoldable summary
           (fun () ->
              let l = List.map (traverse ~fold:true) l in
              Lwd_utils.pack Ui.Ui.pack_y l)
@@ -54,15 +30,15 @@ let ui_of_cbor (c:C.t) =
       )
     | `Map [] -> Lwd.return (W.string "{}")
     | `Map [x,y] ->
-      unfoldable (traverse x) (fun () -> traverse ~fold:false y)
+      W.unfoldable (traverse x) (fun () -> traverse ~fold:false y)
     | `Map l ->
       let summary = Lwd.return @@ W.printf ~attr:A.(fg yellow) "<map(%d)>" (List.length l) in
-      unfoldable summary
+      W.unfoldable summary
         (fun () ->
            let tbl = Lwd_table.make () in
            List.iter (fun (x,y) ->
                let row = Lwd_table.append tbl in
-               let kv = unfoldable (traverse x) (fun () -> traverse ~fold:false y) in
+               let kv = W.unfoldable (traverse x) (fun () -> traverse ~fold:false y) in
                Lwd_table.set row kv)
              l;
            Lwd.join @@ Lwd_table.reduce (Lwd_utils.lift_monoid Ui.Ui.pack_y) tbl)
@@ -71,7 +47,7 @@ let ui_of_cbor (c:C.t) =
     Lwd.map2 Ui.Ui.join_y w_q
       (Nottui_widgets.scroll_area @@ traverse ~fold:true c)
   in
-  Lwd.get quit, w
+  quit, w
 
 let show_file f =
   let cbor = CCIO.with_in f (fun ic -> CCIO.read_all ic |> C.decode) in
