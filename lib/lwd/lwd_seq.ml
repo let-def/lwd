733852ea6ecfa5a765f9be4812f3693e309aa8ca
Frédéric Bour <fred@tarides.com>
2020-11-01 15:12:46 +0100

    Lwd_seq: major bugfix 1/2: dropped join nodes incorrectly stored
    dropped join nodes were stored like leaf nodes

--- lib/lwd/lwd_seq.ml
+++ lib/lwd/lwd_seq.ml
@@ -236,6 +236,7 @@ module Reducer = struct
         if dropped_leaf > -1 then (
           st.dropped.(dropped_leaf) <- b;
           st.dropped_leaf <- dropped_leaf + 1;
+          assert (st.dropped_leaf <= st.dropped_join);
         );
         t'.mark <- mark land lnot both_mask
       ) else if mark = -1 then (
@@ -253,10 +254,11 @@ module Reducer = struct
     | XJoin {a = Join t' as a; l; r; b} as t ->
       let mark = t'.mark in
       if mark land both_mask = old_mask then (
-        let dropped_leaf = st.dropped_leaf in
-        if dropped_leaf > -1 then (
-          st.dropped.(dropped_leaf) <- b;
-          st.dropped_leaf <- dropped_leaf + 1;
+        let dropped_join = st.dropped_join in
+        if dropped_join > -1 then (
+          st.dropped.(dropped_join) <- b;
+          st.dropped_join <- dropped_join - 1;
+          assert (st.dropped_leaf <= st.dropped_join);
         );
         t'.mark <- mark land lnot both_mask;
         unmark_old st l;
@@ -352,8 +354,8 @@ module Reducer = struct
       let nb_dropped = sold.marked - (sold.blocked + snew.blocked) in
       let st = {
         dropped = if get_dropped then Array.make nb_dropped None else [||];
-        dropped_leaf = if get_dropped then 0 else -1;
-        dropped_join = if get_dropped then nb_dropped else -1;
+        dropped_leaf = if get_dropped then 0 else - 1;
+        dropped_join = if get_dropped then nb_dropped - 1 else - 1;
         shared = Array.make (sold.shared + snew.shared) Nil;
         shared_x = Array.make (sold.shared + snew.shared) [];
         shared_index = 0;
